f4202bb90e7707b54e6d5b601f5c128d201e90e4
Mike Smith <gooseisdead@mac.com>
2020-08-31 18:02:50 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -4,6 +4,10 @@ end
 
 def monopoly_with_second_tier
   # When you start writing the implementation for this method, copy the Hash
+  base_hash = { :railroads => {
+    {:pieces => {}, {}, {}, {},
+     :rent_in_dollars => {} }
+    } }
   # you built in the previous method. Run the tests. They will fail, but
   # they'll guide you in how to modify what you just did to have more
   # complexity.
